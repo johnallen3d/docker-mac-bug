40352e9a8b76f27f3902267ac1cb24ea9af47dae
Nikola Kovacs <nikola.kovacs@gmail.com>
2016-07-11 16:52:13 +0200

    reduce example

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-sane = require('sane');
-var watcher = sane('test/', {glob: ['**/*']});
-watcher.on('change', function(filepath, root, stat) { console.log('file changed', filepath); });
-watcher.on('add', function(filepath, root, stat) { console.log('file add', filepath); });
+// sane = require('sane');
+// var watcher = sane('test/', {glob: ['**/*']});
+// watcher.on('change', function(filepath, root, stat) { console.log('file changed', filepath); });
+// watcher.on('add', function(filepath, root, stat) { console.log('file add', filepath); });
+var fs = require('fs');
+var path = require('path');
+
+var dir = 'test/';
+
+fs.watch(dir, function(event, filename) {
+    console.log(event, filename);
+    var fullpath = path.join(dir, filename);
+    fs.lstat(fullpath, function(error, stat) {
+        console.log(error, stat);
+        fs.lstat(fullpath, function(error, stat) {
+            console.log(error, stat);
+        });
+    });
+});
