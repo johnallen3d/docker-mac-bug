0c7fed5895ed574121cc1cf070ed01431fb461a8
Nikola Kovacs <nikola.kovacs@gmail.com>
2016-07-12 11:15:12 +0200

    refactor, use mount with dash in name

--- entry.sh
+++ entry.sh
@@ -6,6 +6,20 @@ echo "starting inotifywait"
 #     inotifywatch -t 1 /var/www/app/test.txt /var/www/other/testother.txt
 # done
 
-trap 'killall -9 inotifywait' QUIT EXIT TERM
+pid=0
 
-inotifywait -m /var/www/app/ /var/www/other/ /var/www/app/test/
+cleanup() {
+    trap '' QUIT EXIT TERM
+    echo "Exiting"
+    if [ $pid -ne 0 ]; then
+        kill $pid
+    fi
+    exit 0
+}
+
+trap cleanup QUIT EXIT TERM
+
+inotifywait -m /var/www/app/ /etc/go-cron /var/www/app/test/ &
+pid=$!
+
+wait $pid
--- test.sh
+++ test.sh
@@ -4,9 +4,11 @@ docker-compose up --build -d
 
 echo "test" >> test.txt
 echo "test other" >> other/testother.txt
+echo "test nested" >> test/test.txt
 
 docker-compose logs
 docker-compose down
 
 echo "" > test.txt
 echo "" > other/testother.txt
+echo "" > test/test.txt
